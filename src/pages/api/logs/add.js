2c379e9ae9abb6de4844e9bb2a63cb0d92f5e46c
Refsi <refsisangkay@gmail.com>
2024-10-17 11:12:53 +0800

    fix: cant checkin if data not exist

--- src/pages/api/logs/add.js
+++ src/pages/api/logs/add.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
         },
       });
 
-      let lastStatus = "CheckIn";
+      let lastStatus = "";
       if (lastLog) {
         lastStatus = lastLog?.status;
       }
